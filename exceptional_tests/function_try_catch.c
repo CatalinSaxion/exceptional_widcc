2bbdd2fa723193e930c4a759414d64ede2693269
catalin.chiprian <chiprian.catalin@autoform2020!>
2024-12-03 09:29:12 +0100

    Modified try catch test

--- exceptional_tests/function_try_catch.c
+++ exceptional_tests/function_try_catch.c
@@ -2,6 +2,7 @@
 
 int calc(int a, int b) {
 	int sum = a + b;
+	int test = 10;
 	try{
 		printf("Hello from try 3 block\n");
 	throw 1;
@@ -15,22 +16,77 @@ int calc(int a, int b) {
 	return sum;
 }
 
-int main() {
-    printf("Hello, World!\n");
-	try {
-		int c = 10;
-		printf("In try c is %d\n", c);
-		throw c;
+int calc1(int a, int b) {
+	int sum = a + b;
+	int test = 10;
+	try{
+		printf("Hello from try 3 block\n");
+	throw 1;
 	}
-	catch(int ex_nr) {
-		printf("caught exception: %d\n", ex_nr);
+	catch(int sum) {
+		printf("caught exception 3: %d\n", sum);
 	}
 	finally {
-		//printf("Hello from finally block\n");
-		printf("In finally c is %d\n", c);
-		c=0;
-		printf("In finally we change c to %d\n", c);
+		printf("Hello from finally 3  block\n");
 	}
-	
+	return sum;
+}
+int calc2(int a, int b) {
+	int sum = a + b;
+	int test = 10;
+	try{
+		printf("Hello from try 3 block\n");
+	throw 1;
+	}
+	catch(int sum) {
+		printf("caught exception 3: %d\n", sum);
+	}
+	finally {
+		printf("Hello from finally 3  block\n");
+	}
+	return sum;
+}
+
+int calc3(int a, int b) {
+	int sum = a + b;
+	int test = 10;
+	try{
+		printf("Hello from try 3 block\n");
+	throw 1;
+	}
+	catch(int sum) {
+		printf("caught exception 3: %d\n", sum);
+	}
+	finally {
+		printf("Hello from finally 3  block\n");
+	}
+	return sum;
+}
+
+void* calc4(int a, int b) {
+	int sum = a + b;
+	int test = 10;
+	try{
+		printf("Hello from try 3 block\n");
+	throw (void*)1;
+	}
+	catch(void* sum) {
+		printf("caught exception 3: %d\n", sum);
+	}
+	finally {
+		printf("Hello from finally 3  block\n");
+	}
+	return sum;
+}
+
+int main() {
+    printf("Hello, World!\n");
+    for (int i = 0; i < 10000; i++) {
+    calc(3,5);
+	calc1(3,5);
+	calc2(4,5);
+	calc3(5,5);
+	calc4(10,4);
+}
     return 0;
 }
